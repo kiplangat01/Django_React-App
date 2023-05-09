5ab1778944c7e9e37785e0d8dbe9787c7ea8ec27
kiplangat01 <meshnobie@gmail.com>
2023-05-09 10:58:27 +0300

    created new templates

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h2>Home page</h2>')
+    return render(request, 'blog/index.html')
 
 def about(request):
-    return HttpResponse('<h2>About page</h2>')
+    return render(request ,'blog/about.html')
  
